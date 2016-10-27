50888144f6e9557670931927c036aebbb75e6e86
Feiko Joosten <feiko_joosten@hotmail.com>
2016-10-27 22:10:38 +0200

    First upload
    Upload of the base Unity project.

---
+++ Unity Project/Mobile Game/Assets/UpdateManager/Scripts/OverridableMonoBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OverridableMonoBehaviour : MonoBehaviour
+{
+	protected virtual void Awake()
+	{
+		UpdateManager.AddItem(this);
+	}
+
+	/// <summary>
+	/// If your class uses the Awake function, please use  protected override void Awake() instead.
+	/// Also don't forget to call OverridableMonoBehaviour.Awake(); first.
+	/// If your class does not use the Awake function, this object will be added to the UpdateManager automatically.
+	/// Do not forget to replace your Update function with public override void UpdateMe()
+	/// </summary>
+	public virtual void UpdateMe() {}
+}
